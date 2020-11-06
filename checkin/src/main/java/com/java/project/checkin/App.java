947717b91d11b8b3c8e91e263ff85fe84e782f4c
Armando Perea <terry900407@gmail.com>
2020-11-05 18:58:26 -0600

    Inicio de projecto

---
+++ checkin/src/main/java/com/java/project/checkin/App.java
@@ -0,0 +1,13 @@
+package com.java.project.checkin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
